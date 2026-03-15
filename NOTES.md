303bfb0665dbd7f116c194c32cb7951635e495ef
ekansh0202 <ekansh.anand02@gmail.com>
2026-03-15 15:07:28 +0530

    Project setup and navbar changes

---
+++ NOTES.md
@@ -0,0 +1,5 @@
+Libraries Used in the project
+
+1. vite
+2. tailwind
+3. dayjs
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,42 @@
+import dayjs from "dayjs";
+import { navIcons, navLinks } from "../constants";
+
+const Navbar = () => {
+
+  return (
+    <nav>
+      <div>
+        <img src="/images/logo.svg" alt="Logo" />
+        <p className="font-bold">Ekansh's Portfolio</p>
+
+        <ul>
+          {navLinks.map(({ id, name }) => {
+            return (
+              <li key={id}>
+                <p>{name}</p>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div>
+        <ul>
+            {
+                navIcons.map(({ id, img}) => {
+                    return(
+                        <li key={id}>
+                            <img src={img} className="icon-hover" alt={`icon-${id}`} />
+                        </li>
+                    )
+                })
+            }
+        </ul>
+        
+        <time>{dayjs().format("ddd MMM D h:mm A")}</time>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
